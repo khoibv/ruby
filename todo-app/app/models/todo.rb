58b541c7c683ba475b0d046b0555a05af8208771
Khoi Bui Van <buivankhoi@gmail.com>
2018-08-27 09:13:07 +0700

    Add relationship in ActiveRecord

--- todo-app/app/models/todo.rb
+++ todo-app/app/models/todo.rb
@@ -1,6 +1,9 @@
 class Todo < ApplicationRecord
 
   belongs_to :user
+  scope :completed, -> {where(status: true)}
+  scope :by_user, ->(username) {where(user_id: User.find_by(name: username))}
+
   validates :todo, length: {maximum: 100}, presence: true
 
 end
--- todo-app/app/models/user.rb
+++ todo-app/app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
 
   has_many :todos
 
-  validates :name, length: {maximum: 50}, presence: true
+
+
+
+  validates :name, length: {maximum: 50}, presence: {message: :name_is_empty}
   validates :address, length: {maximum: 250}, presence: true
 
 end
